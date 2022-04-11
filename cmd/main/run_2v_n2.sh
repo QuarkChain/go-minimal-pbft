d7d9f9de2c34be52e9af25603ef0c731331eca3d
pingke <pingke@quarkchain.org>
2022-04-11 10:30:02 +0800

    change two validator to 4 validator for peer limited test

--- cmd/main/run_2v_n2.sh
+++ cmd/main/run_2v_n2.sh
@@ -9,7 +9,8 @@ rm -rf ./datadir_2v_n2 &&  \
   --validatorSet=0x564D965830b6081506c6de0625F089F751Af134a \
   --validatorSet=0x0a700e9B59d92259C68E50a978c851214916BE52 \
   --validatorSet=0x00D6bF1DbB497e8C638330ffb351B21F950934e0 \
+  --validatorSet=0xcad3ADBB6985D33e85e10fdAdE8E5b132Ba4E965 \
   --bootstrap /ip4/127.0.0.1/udp/8999/quic/p2p/12D3KooWEZ94qZgJgUNYiLwXahknkniYgozxw5eocijZJkew6Mj5 \
   --port 8997 \
-  --maxPeerCount 1 \
+  --maxPeerCount 2 \
   $@
