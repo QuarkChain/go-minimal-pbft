ad14c42eadd7014bb4fe537d1397550aa75b0864
pingke <pingke@quarkchain.org>
2022-04-13 09:33:34 +0800

    update log

--- p2p/conngater.go
+++ p2p/conngater.go
@@ -46,7 +46,7 @@ func (cg *connGater) InterceptPeerDial(p peer.ID) (allow bool) {
 }
 
 func (cg *connGater) InterceptAddrDial(p peer.ID, a ma.Multiaddr) (allow bool) {
-	log.Info("InterceptPeerDial", "peer id", p)
+	log.Info("InterceptAddrDial", "peer id", p)
 	return cg.h.Network().Connectedness(p) == network.Connected || !cg.isPeerAtLimit()
 }
 
@@ -55,7 +55,7 @@ func (cg *connGater) InterceptAccept(cma network.ConnMultiaddrs) (allow bool) {
 }
 
 func (cg *connGater) InterceptSecured(dir network.Direction, p peer.ID, cma network.ConnMultiaddrs) (allow bool) {
-	log.Info("InterceptPeerDial", "peer id", p)
+	log.Info("InterceptSecured", "peer id", p)
 	return cg.h.Network().Connectedness(p) == network.Connected || !cg.isPeerAtLimit()
 }
 
